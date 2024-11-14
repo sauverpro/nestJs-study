baf503c4ca1d33d22fa101010d5414634322700c
sauverpro <unsauvechriss@gmail.com>
2024-11-14 19:08:30 +0200

    this is user controller that contains all user's route

---
+++ src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+  @Get()
+  findAll(@Query('role') role?: 'ADMIN' | 'INTERN' | 'ENGINEER') {
+    return this.userService.findAll(role);
+  }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+  @Post()
+  create(
+    @Body(ValidationPipe)
+    createUserDto: CreateUserDto,
+  ) {
+    return this.userService.create(createUserDto);
+  }
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) userUpdateDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, userUpdateDto);
+  }
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
+  }
+}
